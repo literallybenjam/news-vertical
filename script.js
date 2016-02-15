c8d25604c18bd9ca36d1a422a42098319d66f524
literallybenjam <bjmns@me.com>
2016-02-15 04:22:24 -0800

    Better touch processing

--- script.js
+++ script.js
@@ -17,7 +17,8 @@ var News = {
         youtube: '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="text" viewBox="0 0 68 48"><path mask="url(#news-metadata-youtube-mask)" d="m .66,37.62 c 0,0 .66,4.70 2.70,6.77 2.58,2.71 5.98,2.63 7.49,2.91 5.43,.52 23.10,.68 23.12,.68 .00,-1.3e-5 14.29,-0.02 23.81,-0.71 1.32,-0.15 4.22,-0.17 6.81,-2.89 2.03,-2.07 2.70,-6.77 2.70,-6.77 0,0 .67,-5.52 .67,-11.04 l 0,-5.17 c 0,-5.52 -0.67,-11.04 -0.67,-11.04 0,0 -0.66,-4.70 -2.70,-6.77 C 62.03,.86 59.13,.84 57.80,.69 48.28,0 34.00,0 34.00,0 33.97,0 19.69,0 10.18,.69 8.85,.84 5.95,.86 3.36,3.58 1.32,5.65 .66,10.35 .66,10.35 c 0,0 -0.55,4.50 -0.66,9.45 l 0,8.36 c .10,4.94 .66,9.45 .66,9.45 z"/></svg>'
     },
     processTouch: undefined,
-    processScroll: undefined
+    processScroll: undefined,
+    processed_metadata_elements: document.documentElement.getElementsByClassName("news-metadata-processed")
 }
 
 News.getMetadataElements = function() {
@@ -107,8 +108,11 @@ News.processScroll = function(e) {
 }
 
 News.processTouch = function(e) {
+    var i;
     if (News.features.metadata && News.is_initialized.metadata) {
-        if (e.target.hasAttribute("data-news-metadata-activated")) e.target.removeAttribute("data-news-metadata-activated");
-        else if (e.target.classList.contains("news-metadata-processed")) e.target.setAttribute("data-news-metadata-activated", "");
+        for (i = 0; i < News.processed_metadata_elements.length; i++) {
+            if (News.processed_metadata_elements.item(i).hasAttribute("data-news-metadata-activated")) News.processed_metadata_elements.item(i).removeAttribute("data-news-metadata-activated");
+        }
+        if (e.target.classList.contains("news-metadata-processed")) e.target.setAttribute("data-news-metadata-activated", "");
     }
 }
